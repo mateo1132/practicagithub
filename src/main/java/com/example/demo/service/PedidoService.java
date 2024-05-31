19d4d0af5e8c85c259f20ba108f923200d3d171f
Mateo <Mateo@DESKTOP-0R5FAAA>
2024-05-31 17:45:22 +0200

    Comentarios javadoc para la clase AppController y PedidoService

--- src/main/java/com/example/demo/service/PedidoService.java
+++ src/main/java/com/example/demo/service/PedidoService.java
@@ -6,7 +6,13 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+/**
+ * @author Mateo
+ */
 
+/**
+ * Servicio utilizado en la clase AppController que contine los metodos utilizados en el controlador
+ */
 @Service
 public class PedidoService {
 	
@@ -15,21 +21,36 @@ public class PedidoService {
 	
 	private HashMap<Long,Pedido> pedidos = new HashMap<>();
 	
-	
+	/**
+	 * Metodo para crear un pedido
+	 * @param id El id del pedido
+	 * @param pedido El objeto pedido 
+	 */
 	public void crearPedido(Long id,Pedido pedido) {
 		if (userService.existeUsuario(pedido.getidUsuario())) {
 			pedidos.put(id, pedido);
 		}
 	}
+	/**
+	 * Metodo para buscar un pedido
+	 * @param id El id del pedido 
+	 * @return devuelve el pedido que coincide con el id
+	 */
 	
 	public Pedido buscarPedido(Long id) {
 		return pedidos.get(id);
 	}
-	
+	/**
+	 * Metodo que sirve para procesar un pedido
+	 * @param id El id del pedido
+	 */
 	public void procesarPedido(Long id) {
 		pedidos.get(id).setEstado(StatusPedido.PROCESADO);
 	}
-	
+	/**
+	 * Metodo para borrar un pedido
+	 * @param id El id del pedido 
+	 */
 	public void borrarPedido(Long id) {
 		if (pedidos.get(id).getEstado()!= StatusPedido.PROCESADO) {
 			pedidos.get(id).setEstado(StatusPedido.BORRADO);
